b418937485dc4cd293c9e1ec35ee4db2d0cc0c53
Risyardi Iswanto <elyeel@yahoo.com>
2021-02-23 16:48:53 -0800

    added booklist.js, adding addbook.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+	ApolloClient,
+	ApolloProvider,
+	InMemoryCache,
+	useQuery
+} from '@apollo/client';
+import BookList from './components/BookList';
+
+// apollo client setup
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/graphql',
+	cache: new InMemoryCache()
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<div id="main">
+				<h1>Risyardi's GraphQL</h1>
+				<BookList client={client} useQuery={useQuery} />
+			</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
